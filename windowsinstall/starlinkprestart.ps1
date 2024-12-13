4ce52c9e3aaa60d64aef8a92d8ae952523fe730e
Jesse Newell <ensjesse@msn.com>
2024-12-13 00:55:37 -0800

    Delete windowsinstall directory
    Clean up: remove files not needed for the project. Moving this fork to Linux only. Docker images are planned for future support.

--- windowsinstall/starlinkprestart.ps1
+++
@@ -1,3 +0,0 @@
-﻿$StarlinkFolder="C:\users\$env:USERNAME\documents\StarlinkScripts"
-$key=Get-Content $Starlinkfolder\thekey.txt
-powershell.exe -noprofile -executionpolicy bypass -file "$StarlinkFolder\Starlinkstatusstarter.ps1" 
